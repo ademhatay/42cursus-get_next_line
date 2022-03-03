32a7abde1a69626c272b8eee0f603fdb4890881e
Adem Hatay <ahatay@k2m01s04.42kocaeli.com.tr>
2022-03-04 02:13:10 +0300

    nextline ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: ahatay <ahatay@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 22:13:19 by ahatay            #+#    #+#             */
-/*   Updated: 2022/03/03 23:29:05 by ahatay           ###   ########.fr       */
+/*   Updated: 2022/03/04 01:16:50 by ahatay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
@@ -23,10 +24,10 @@
 
 char	*get_next_line(int fd);
 char	*ft_read_to_left_str(int fd, char *left_str);
-size_t	ft_strlen(char *str);
+size_t	ft_strlen(char *s);
 char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_get_line(char left_str);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_get_line(char *left_str);
 char	*ft_new_left_str(char *left_str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,34 +6,40 @@
 /*   By: ahatay <ahatay@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 22:02:59 by ahatay            #+#    #+#             */
-/*   Updated: 2022/03/03 23:33:17 by ahatay           ###   ########.fr       */
+/*   Updated: 2022/03/04 01:12:40 by ahatay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(char *str)
+size_t	ft_strlen(char *s)
 {
 	size_t	len;
 
 	len = 0;
-	while (str[len] != '\0')
+    if (!s)
+        return (0);
+	while (s[len] != '\0')
 		len++;
 	return (len);
 }
 
-char	*ft_strchr(char *s, int c)
+char    *ft_strchr(char *s, int c)
 {
-	size_t	counter;
+    int    i;
 
-	counter = 0;
-	while (counter <= ft_strlen(s))
-	{
-		if (s[counter] == (unsigned char)c)
-			return ((char *)s + counter);
-		counter++;
-	}
-	return (0);
+    i = 0;
+    if (!s)
+        return (0);
+    if (c == '\0')
+        return ((char *)&s[ft_strlen(s)]);
+    while (s[i] != '\0')
+    {
+        if (s[i] == (char) c)
+            return ((char *)&s[i]);
+        i++;
+    }
+    return (0);
 }
 
 char	*ft_strjoin(char *left_str, char *buff)
@@ -64,32 +70,57 @@ char	*ft_strjoin(char *left_str, char *buff)
 	return (str);
 }
 
-char	*ft_get_line(char left_str)
+char    *ft_get_line(char *left_str)
 {
-	int		count;
-	char	*str;
+    int        i;
+    char    *str;
 
-	count = 0;
-	if (!left_str[count])
-		return (NULL);
-	while (left_str[count] && left_str[count] != \n'')
-		count++;
-	str = malloc(sizeof(char) * (count + 2));
-	if (!str)
-		return (NULL);
-	count = 0;
-	while (left_str[count] && left_str[count] != '\n')
-	{
-		str[count] = left_str[count];
-		count++;
-	}
-	if (left_str[count] != '\n')
-	{
-		str[count] = left_str[count];
-		count++;
-	}
-	str[count] = '\0';
-	return (str);
+    i = 0;
+    if (!left_str[i])
+        return (NULL);
+    while (left_str[i] && left_str[i] != '\n')
+        i++;
+    str = (char *)malloc(sizeof(char) * (i + 2));
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (left_str[i] && left_str[i] != '\n')
+    {
+        str[i] = left_str[i];
+        i++;
+    }
+    if (left_str[i] == '\n')
+    {
+        str[i] = left_str[i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
 }
 
-char	*ft_new_left_str(char *str)
+char    *ft_new_left_str(char *left_str)
+{
+    int        i;
+    int        j;
+    char    *str;
+
+    i = 0;
+    while (left_str[i] && left_str[i] != '\n')
+        i++;
+    if (!left_str[i])
+    {
+        free(left_str);
+        return (NULL);
+    }
+    str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+    if (!str)
+        return (NULL);
+    i++;
+    j = 0;
+    while (left_str[i])
+        str[j++] = left_str[i++];
+    str[j] = '\0';
+    free(left_str);
+    return (str);
+}
+
